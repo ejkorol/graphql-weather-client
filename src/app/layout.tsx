9a30d65722f123b5ef05348c10e91810435d3414
Jason Korol <ejkorol@gmail.com>
2024-09-29 14:23:31 -0400

    creates provider for next themes, creates static footer and creates theme switcher components. implements into root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,28 @@
 import type { Metadata } from "next";
 import { inter } from "@/utils/fonts";
 import "./globals.css";
+import { Providers } from "./providers/Provider";
+import { ThemeSwitcher, Footer } from "@/components/ui";
 
 export const metadata: Metadata = {
   title: "GraphQL Weather Client",
   description: "Frontend implementation of the API",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
+const RootLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <main>{children}</main>
+        <main>
+          <Providers>
+            <ThemeSwitcher />
+            {children}
+            <Footer />
+          </Providers>
+        </main>
       </body>
     </html>
   );
-}
+};
+
+export default RootLayout;
